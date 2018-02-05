465ca2bba28cccffe4c44f8039ec6dd4ae11fdc8
Lucas Alves <luk4z_7@hotmail.com>
2018-02-05 09:49:38 -0300

    Generated two binary files, darwin and linux

--- docker/golang/build.sh
+++ docker/golang/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 echo "[build.sh:building binary]"
-cd $BUILDPATH && go build -o /app && rm -rf /tmp/*
 
-cp /app /usr/bin/
-chmod u+x /usr/bin/app
+cd $BUILDPATH && GOOS=linux GOARCH=amd64 go build -o ckp_linux86_64 && rm -rf /tmp/*
+cd $BUILDPATH && GOOS=darwin GOARCH=amd64 go build -o ckp_darwinx86_64 && rm -rf /tmp/*
+
+cp ckp_linux86_64 /usr/bin/
+cp ckp_darwinx86_64 /usr/bin/
+
+chmod u+x /usr/bin/ckp_linux86_64
 
 echo "[build.sh:launching binary]"
-/app
+ckp_linux86_64
