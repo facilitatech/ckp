c528c6b4951d501ccd6eeb3d91c22789d3a37e09
Lucas Alves <luk4z_7@hotmail.com>
2018-02-24 10:30:42 -0300

    Changed build.sh to work with binary to linux.

--- docker/golang/build.sh
+++ docker/golang/build.sh
@@ -2,13 +2,13 @@
 set -e
 echo "[build.sh:building binary]"
 
-cd $BUILDPATH && GOOS=linux GOARCH=amd64 go build -o ckp_linux86_64 && rm -rf /tmp/*
-cd $BUILDPATH && GOOS=darwin GOARCH=amd64 go build -o ckp_darwinx86_64 && rm -rf /tmp/*
+# linux86_64
+cd $BUILDPATH && \
+   GOOS=linux GOARCH=amd64 go build -o ckp && \
+   mv ./ckp /usr/bin/ && \
+   rm -rf /tmp/*
 
-cp ckp_linux86_64 /usr/bin/
-cp ckp_darwinx86_64 /usr/bin/
-
-chmod u+x /usr/bin/ckp_linux86_64
+chmod u+x /usr/bin/ckp
 
 echo "[build.sh:launching binary]"
-ckp_linux86_64
+ckp
